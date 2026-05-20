8b3fd6ffef71b375e7b0771ae3c2c3415120eca2
usamabinmah12 <usamabinmahbub13@gmail.com>
2026-05-20 13:47:47 +0600

    get and post has been set up for

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-const express =  require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-app.use(cors());
-app.use(express());
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const port = 5000;
 
+app.use(cors());
+app.use(express.json()); 
 
+const uri = "mongodb+srv://petnest:CBKpifrvVXPT0E8d@cluster0.wfpggc0.mongodb.net/?appName=Cluster0";
 
-app.get('/' , (req , res) => {
-    res.send('Bismillahir Rahmanir Rahim');
-})
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-app.listen(port , () => {
-    console.log(`App listening at port ${port}`);
-})
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("petnest");
+    const petsCollection = db.collection("pets"); 
+
+    app.get("/pets", async (req, res) => {
+      const data = await petsCollection.find().toArray();
+      res.json(data);
+    });
+
+    app.post("/pets", async (req, res) => {
+      const pet = req.body;
+
+      const result = await petsCollection.insertOne(pet);
+
+      res.json(result);
+    });
+
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+run();
+
+app.get("/", (req, res) => {
+  res.send("Bismillahir Rahmanir Rahim");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
